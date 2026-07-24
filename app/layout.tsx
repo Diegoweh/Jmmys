6f6421a37b2e38333b8220162c6f57241ba2bc1d
Diegoweh <105457980+Diegoweh@users.noreply.github.com>
2026-07-24 09:58:55 -0700

    pixel added

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,6 +95,29 @@ export default function RootLayout({
             gtag('config', 'G-NG7VWR0K6D');
           `}
         </Script>        
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1763515634691209');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1763515634691209&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
 
         <CartProvider>
           {/*<PromoDevTools />*/}
